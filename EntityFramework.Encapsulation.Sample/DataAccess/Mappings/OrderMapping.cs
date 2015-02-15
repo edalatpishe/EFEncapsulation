c0970e84b0d52c3d0128a76b58bfbaab38ab2d40
hadi <ahmadi.hadi@outlook.com>
2015-02-15 23:22:54 +0330

    Sample optional Order changed to required

--- EntityFramework.Encapsulation.Sample/DataAccess/Mappings/OrderMapping.cs
+++ EntityFramework.Encapsulation.Sample/DataAccess/Mappings/OrderMapping.cs
@@ -9,18 +9,18 @@ using EntityFramework.Encapsulation.Sample.Domain.Model;
 
 namespace EntityFramework.Encapsulation.Sample.DataAccess.Mappings
 {
-    public class OrderMapping : EntityTypeConfiguration<Order>
+public class OrderMapping : EntityTypeConfiguration<Order>
+{
+    public OrderMapping()
     {
-        public OrderMapping()
-        {
-            ToTable("Order")
-                .HasKey(a => a.Id)
-                .Property(a => a.Id)
-                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.None);
+        ToTable("Order")
+            .HasKey(a => a.Id)
+            .Property(a => a.Id)
+            .HasDatabaseGeneratedOption(DatabaseGeneratedOption.None);
 
-            this.PrivateProperty(a => a.CreateDateTime);
-            this.PrivateProperty(a => a.CustomerName);
-            this.HasManyPrivate(a => a.OrderItems).WithOptional().Map(a=>a.MapKey("OrderId"));
-        }
+        this.PrivateProperty(a => a.CreateDateTime);
+        this.PrivateProperty(a => a.CustomerName);
+        this.HasManyPrivate(a => a.OrderItems).WithRequired().Map(a=>a.MapKey("OrderId"));
     }
+}
 }
